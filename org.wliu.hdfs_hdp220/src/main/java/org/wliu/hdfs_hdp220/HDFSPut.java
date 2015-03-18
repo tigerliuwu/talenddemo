be3d98fac317b2d42637a763d862e56d1456efac
Wu Liu <wliu@talend.com>
2015-03-18 19:02:59 +0800

    add another 2 outputstream support

--- org.wliu.hdfs_hdp220/src/main/java/org/wliu/hdfs_hdp220/HDFSPut.java
+++ org.wliu.hdfs_hdp220/src/main/java/org/wliu/hdfs_hdp220/HDFSPut.java
@@ -11,11 +11,21 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.compress.BZip2Codec;
 import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.util.ReflectionUtils;
 
 public class HDFSPut {
 	
 	public static final String FSNAME = "talend";
+	
+	public static OutputStream getFSOutputStream(Configuration conf, String hdfsDest) throws IOException, InterruptedException, URISyntaxException {
+		FileSystem fs = null;
+		fs = FileSystem.get(new java.net.URI(conf.get("fs.default.name")), conf, FSNAME);
+		
+		Path path_dest = new Path(hdfsDest);
+		FSDataOutputStream fsOut = fs.create(path_dest, true);
+		return fsOut;
+	}
 	
 	public static OutputStream getBZip2OutStream(Configuration conf, String hdfsDest) throws IOException, InterruptedException, URISyntaxException {
 		FileSystem fs = null;
@@ -24,6 +34,18 @@ public class HDFSPut {
 		Path path_dest = new Path(hdfsDest);
 		FSDataOutputStream fsOut = fs.create(path_dest, true);
 		CompressionCodec codecOutput = ReflectionUtils.newInstance(BZip2Codec.class, conf);
+		CompressionOutputStream cmprOutput = codecOutput.createOutputStream(fsOut);
+		return cmprOutput;
+	}
+	
+	public static OutputStream getGZipOutStream(Configuration conf, String hdfsDest) throws IOException, InterruptedException, URISyntaxException {
+		FileSystem fs = null;
+		fs = FileSystem.get(new java.net.URI(conf.get("fs.default.name")), conf, FSNAME);
+		
+		Path path_dest = new Path(hdfsDest);
+		FSDataOutputStream fsOut = fs.create(path_dest, true);
+		GzipCodec codecOutput = new GzipCodec();
+		codecOutput.setConf(conf);
 		CompressionOutputStream cmprOutput = codecOutput.createOutputStream(fsOut);
 		return cmprOutput;
 	}
